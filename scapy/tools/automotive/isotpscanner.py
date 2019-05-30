97abe45bc97c7b4ae2f597034b9ab63016d60895
Nils Weiss <nils@we155.de>
2019-05-30 12:13:54 +0200

    add todos

--- scapy/tools/automotive/isotpscanner.py
+++ scapy/tools/automotive/isotpscanner.py
@@ -1,3 +1,11 @@
+#! /usr/bin/env python
+
+# This file is part of Scapy
+# See http://www.secdev.org/projects/scapy for more information
+# Copyright (C) Nils Weiss <nils@we155.de>
+# Copyright (C) Alexander Schroeder <alexander1.schroeder@st.othr.de>
+# This program is published under a GPLv2 license
+
 import argparse
 
 import scapy.modules.six as six
@@ -8,12 +16,14 @@ if six.PY2:
     conf.contribs['CANSocket'] = {'use-python-can': True}
 
 from scapy.contrib.cansocket import CANSocket
-from scapy.contrib.isotp import isotpscan, KeepAwakeThread
+from scapy.contrib.isotp import ISOTPScan, KeepAwakeThread
 
 if "python_can" in CANSocket.__module__:
+    #todo use argparse parameter for iface and add example (socketcan, vector, ...) to help
     import can as python_can
     new_can_socket = lambda iface: \
-        CANSocket(iface=python_can.interface.Bus(bustype='socketcan', channel=iface))
+        CANSocket(iface=python_can.interface.Bus(bustype='socketcan',
+                                                 channel=iface))
 else:
     new_can_socket = lambda iface: CANSocket(iface)
 
@@ -25,22 +35,28 @@ def main():
     piso = False
     awake_interface = ""
 
+#Todo: rename Scan for ISOTP-Sockets
     parser = argparse.ArgumentParser(description="Scan for active "
                                                  "ISOTP-Addresses.",
                                      prog="ISOTP Scanner",
-                                     usage="isotpscanner.py startID endID "
+                                     usage="ISOTPScanner.py startID endID "
                                            "interface [-flags]")
     parser.add_argument("startID", type=lambda x: int(x, 16),
                         help="Start scan at this ID (hex)"),
     parser.add_argument("endID", type=lambda x: int(x, 16),
                         help="End scan at this ID (hex)")
+    #Todo: interfacestring has to include python_can strings
+    #Todo: We need tests for that.
     parser.add_argument("interface", type=str,
                         help="CAN interface for the scan")
+    #Todo: Add an optional string parameter for the keep alive interface
+    #Todo: If nothing specified, we use the same interface for keepAlive and scan
     parser.add_argument("-e", "--extended", action="store_true",
                         help="Include extended ID's to scan.")
     parser.add_argument("-k", "--keep_alive", type=str,
                         help="'Keep alive' - \
-                            Send a periodic dummy-packet to specified interface.")
+                            Send a periodic dummy-packet to the "
+                             "specified interface.")
     parser.add_argument("-eo", "--extended_only", action="store_true",
                         help="Scan only with \
                             extended ID's.")
@@ -78,8 +94,9 @@ def main():
 
     # scan normal ID's
     if not extended_only:
-        print("Start scan (" + hex(args.startID) + " - " + hex(args.endID) + ")")
-        result = isotpscan(cansocket_communication,
+        print("Start scan (" + hex(args.startID) + " - " +
+              hex(args.endID) + ")")
+        result = ISOTPScan(cansocket_communication,
                            range(args.startID, args.endID + 1),
                            extended_addressing=False,
                            noise_listen_time=noise_listen_time,
@@ -90,7 +107,7 @@ def main():
     if extended or extended_only:
         print("Start scan with extended ID's (" + hex(args.startID) +
               " - " + hex(args.endID) + ")")
-        result_extended = isotpscan(cansocket_communication,
+        result_extended = ISOTPScan(cansocket_communication,
                                     range(args.startID, args.endID + 1),
                                     extended_addressing=True,
                                     noise_listen_time=noise_listen_time,

ccf27609a711e95668174d36325af3666a1ced3b
superuserx <schroeder.alxndr@web.de>
2019-06-11 11:52:03 +0200

    Refactoring

--- scapy/tools/automotive/isotpscanner.py
+++ scapy/tools/automotive/isotpscanner.py
@@ -7,6 +7,7 @@
 # This program is published under a GPLv2 license
 
 import argparse
+from argparse import RawTextHelpFormatter
 
 import scapy.modules.six as six
 from scapy.config import conf
@@ -21,80 +22,68 @@ from scapy.utils import PeriodicSenderThread
 from scapy.contrib.cansocket import CANSocket, PYTHON_CAN
 from scapy.contrib.isotp import ISOTPScan
 
-if PYTHON_CAN:
-    #todo use argparse parameter for iface and add example (socketcan, vector, ...) to help
-    import can
-    new_can_socket = lambda iface: \
-        CANSocket(iface=can.interface.Bus(bustype='socketcan',
-                                                 channel=iface))
-else:
-    new_can_socket = CANSocket
-
 
 def main():
     extended = False
-    keep_awake = False
     extended_only = False
     piso = False
-    awake_interface = ""
-
-#Todo: rename Scan for ISOTP-Sockets
-    parser = argparse.ArgumentParser(description="Scan for active "
-                                                 "ISOTP-Addresses.",
+    parser = argparse.ArgumentParser(description="Scan for open "
+                                                 "ISOTP-Sockets.",
+                                     formatter_class=RawTextHelpFormatter,
                                      prog="ISOTP Scanner",
-                                     usage="ISOTPScanner.py startID endID "
-                                           "interface [-flags]",
-                                     epilog="python2 -m scapy.tools.automotive.isotpscanner 0 1 \"can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)\"")
+                                     usage="ISOTPScanner.py interface startID endID [-flags] ",
+                                     epilog="Example of use:\n\n"
+                                            "Python2 or Windows:\n"
+                                            "python2 -m scapy.tools.automotive.isotpscanner "
+                                            "\"can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)"
+                                            " 0 1 \"\n\n"
+                                            "Python3 on Linux:\n"
+                                            "python3 -m scapy.tools.automotive.isotpscanner can0 0 1")
+    parser.add_argument("interface", type=str,
+                        help="CAN interface for the scan.\n"
+                             "Depends on used interpreter and system,\n"
+                             "see examples below.")
     parser.add_argument("startID", type=lambda x: int(x, 16),
                         help="Start scan at this ID (hex)"),
     parser.add_argument("endID", type=lambda x: int(x, 16),
                         help="End scan at this ID (hex)")
-    #Todo: interfacestring has to include python_can strings
-    #Todo: We need tests for that.
-    parser.add_argument("interface", type=str,
-                        help="CAN interface for the scan")
-    #Todo: Add an optional string parameter for the keep alive interface
-    #Todo: If nothing specified, we use the same interface for keepAlive and scan
+    parser.add_argument("-n", "--noise_listen_time", type=int, default=10,
+                        help="Set seconds listening for noise before scan.")
     parser.add_argument("-e", "--extended", action="store_true",
                         help="Include extended IDs to scan.")
-    parser.add_argument("-k", "--keep_alive", type=str,
-                        help="'Keep alive' - \
-                            Send a periodic dummy-packet to the "
-                             "specified interface.")
     parser.add_argument("-eo", "--extended_only", action="store_true",
-                        help="Scan only with \
-                            extended IDs.")
+                        help="Scan only with extended IDs.")
     parser.add_argument("-p", "--piso", action="store_true",
                         help="Print 'Copy&Paste'-ready ISOTPSockets.")
 
     args = parser.parse_args()
 
     scan_interface = args.interface
+    if "can.interface.Bus" in scan_interface:
+        if PYTHON_CAN:
+            import can
+            iface = eval(scan_interface)
+            def new_can_socket(iface):
+                return CANSocket(iface=iface)
+        else:
+            print("Wrong interface type.")
+            return
+    else:
+        if PYTHON_CAN:
+            print("Wrong interface type.")
+        else:
+            def new_can_socket(iface):
+                return CANSocket(iface)
+
     if args.extended:
         extended = True
     if args.extended_only:
         extended_only = True
-    if args.keep_alive:
-        keep_awake = True
-        awake_interface = args.keep_alive
     if args.piso:
         piso = True
 
-    # Seconds to listen to noise
-    noise_listen_time = 10
-
     # Interface for communication
     cansocket_communication = new_can_socket(scan_interface)
-
-    # Keep ECU awake
-    awake_thread = None
-    if keep_awake:
-        dummy_pkt = CAN(identifier=0x123,
-                        data=b'\xaa\xbb\xcc\xdd\xee\xff\xaa\xbb')
-        cansocket_keep_awake = new_can_socket(awake_interface)
-        awake_thread = PeriodicSenderThread(cansocket_keep_awake,
-                                            dummy_pkt)
-        awake_thread.start()
 
     # scan normal IDs
     if not extended_only:
@@ -103,9 +92,9 @@ def main():
         result = ISOTPScan(cansocket_communication,
                            range(args.startID, args.endID + 1),
                            extended_addressing=False,
-                           noise_listen_time=noise_listen_time,
+                           noise_listen_time=args.noise_listen_time,
                            output_format="code" if piso else "text")
-        print("Scan: " + result)
+        print("Scan: " + str(result))
 
     # scan extended IDs
     if extended or extended_only:
@@ -114,13 +103,10 @@ def main():
         result_extended = ISOTPScan(cansocket_communication,
                                     range(args.startID, args.endID + 1),
                                     extended_addressing=True,
-                                    noise_listen_time=noise_listen_time,
+                                    noise_listen_time=args.noise_listen_time,
                                     output_format="code" if piso else "text")
-        print("Extended scan: " + result_extended)
-
-    # Stop "stay awake"-traffic
-    if keep_awake:
-        awake_thread.stop()
+                                    #output_format=None)
+        print("Extended scan: " + str(result_extended))
 
 
 if __name__ == '__main__':
